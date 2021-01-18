e04f27de27a8f7973e6e6d3a5f00f19c0b6bbc80
Gerald <gprmarquez@gmail.com>
2021-01-18 01:19:18 -0800

    got chrome extension running and reading current url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+/*global chrome*/
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const url = tabs[0].url;
+      setUrl(url);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>URL: </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {url}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
